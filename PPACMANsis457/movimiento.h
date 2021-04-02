c47bc2dba0d63e83043d37564eb86bfb1c67d5f0
JAGARCIAV <armando.jagv80@gmail.com>
2021-04-02 15:56:38 -0400

    movimientos de pacman

--- PPACMANsis457/movimiento.h
+++ PPACMANsis457/movimiento.h
@@ -1,5 +1,13 @@
 #pragma once
 
+//movimiento con teclas
+#define TECLA_ARRIBA	'w'
+#define TECLA_ABAJO		's'	
+#define TECLA_ABAJO		'a'	
+#define TECLA_ABAJO		'd'	
+
+
+//Direccion de pacman
 enum DireccionMovimiento {
 	MOVER_ARRIBA,
 	MOVER_ABAJO,
